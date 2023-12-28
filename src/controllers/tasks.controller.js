0d8657d289793baf9d7951bf5eea411616514e75
eslam3400 <eslam3400@gmail.com>
2023-12-28 06:26:48 +0200

    add most away chapter review

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -32,13 +32,51 @@ async function createUserTasks(req, res) {
 
 async function getUserTasks(req, res) {
   try {
-    const tasks = await db.Task.findAll({ where: { userId: req.userId } });
-    console.log(req.userId);
+    const tasks = await db.Task.findAll({ where: { userId: req.userId }, order: [["updatedAt", "DESC"]], raw: true });
+    const chapterTasks = await db.TaskChapter.findAll({
+      attributes: ['chapter', 'page', 'updatedAt'],
+      order: [['chapter'], ['page']],
+      raw: true,
+    });
+
+    const groupedByChapter = chapterTasks.reduce((result, task) => {
+      if (!result[task.chapter]) {
+        result[task.chapter] = {
+          chapter: task.chapter,
+          pages: [],
+        };
+      }
+
+      result[task.chapter].pages.push({
+        page: task.page,
+        updatedAt: task.updatedAt,
+      });
+
+      return result;
+    }, {});
+
+    const formattedResult = Object.values(groupedByChapter).map(chapter => ({
+      chapter: chapter.chapter,
+      pages: chapter.pages.sort((a, b) => new Date(b.updatedAt) - new Date(a.updatedAt)),
+    }));
+
+    const orderedResult = [];
+
+    for (const chapter of formattedResult) {
+      for (const page of chapter.pages) {
+        const task = tasks.find(x => x.page == page.page);
+        if (!task) page.updatedAt = new Date();
+        else page.updatedAt = task?.updatedAt;
+      }
+      orderedResult.push(chapter.pages.sort((a, b) => new Date(a.updatedAt) - new Date(b.updatedAt)));
+    }
+
     const data = {
       saved: [],
       within7Days: [],
       within10Days: [],
-      mustReview: []
+      mustReview: [],
+      chapterLatestReview: orderedResult.map((x, i) => ({ chapter: i + 1, mostAwayDate: x[0].updatedAt })),
     }
     for (const task of tasks) {
       const lastUpdateDate = new Date(task.updatedAt);
@@ -57,6 +95,7 @@ async function getUserTasks(req, res) {
     }
     return res.status(200).json({ data });
   } catch (error) {
+    console.log(error)
     res.status(500).json({ msg: 'Server error' });
   }
 }
