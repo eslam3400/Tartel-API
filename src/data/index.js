3b3555e719efd24ba3ef628b8dfa2f8939a09db9
eslam3400 <eslam3400@gmail.com>
2024-07-22 12:15:18 +0300

    add pool for database connections

--- src/data/index.js
+++ src/data/index.js
@@ -18,6 +18,12 @@ const sequelize = new Sequelize(
         rejectUnauthorized: false, // For self-signed certificates
       },
     },
+    pool: {
+      max: 5, // Maximum number of connection in pool
+      min: 0, // Minimum number of connection in pool
+      acquire: 30000, // The maximum time, in milliseconds, that pool will try to get connection before throwing error
+      idle: 10000 // The maximum time, in milliseconds, that a connection can be idle before being released
+    }
   }
 );
 
