063d29313035e83286c598f6fc77e5a8ff4bbe5d
Eslam Magdy <eslam3400@gmail.com>
2024-07-30 20:10:13 +0300

    increase db bool

--- src/data/index.js
+++ src/data/index.js
@@ -15,14 +15,14 @@ const sequelize = new Sequelize(
     dialectOptions: {
       ssl: {
         require: true,
-        rejectUnauthorized: false, // For self-signed certificates
+        rejectUnauthorized: false
       },
     },
     pool: {
-      max: 5, // Maximum number of connection in pool
-      min: 0, // Minimum number of connection in pool
-      acquire: 30000, // The maximum time, in milliseconds, that pool will try to get connection before throwing error
-      idle: 10000 // The maximum time, in milliseconds, that a connection can be idle before being released
+      max: 10,
+      min: 0,
+      acquire: 60000,
+      idle: 10000
     }
   }
 );
