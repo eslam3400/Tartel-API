4d194fd763950533759abce66b3de7e4daf62f8d
Eslam Magdy <eslam3400@gmail.com>
2024-09-19 01:05:23 +0300

    fix get options

--- src/routes/option.routes.js
+++ src/routes/option.routes.js
@@ -5,7 +5,7 @@ const authMiddleware = require('../middlewares/auth.middleware');
 
 router.post('/', authMiddleware, optionController.create);
 // router.get('/', authMiddleware, optionController.getAll);
-router.get('/:id', authMiddleware, optionController.getByKey);
+router.get('/:key', authMiddleware, optionController.getByKey);
 // router.put('/:id', authMiddleware, optionController.update);
 // router.delete('/:id', authMiddleware, optionController.remove);
 
